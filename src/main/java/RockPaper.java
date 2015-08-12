1f84bca503b8ef64ce879fd18ba45c3b0476b130
jocelyn alsdorf & colleen minor & Teresa Fletcher <student@epicodus.com>
2015-08-12 16:44:52 -0700

    Added css and js files,jquery to come

--- src/main/java/RockPaper.java
+++ src/main/java/RockPaper.java
@@ -8,6 +8,7 @@ import static spark.Spark.*;
 
 public class RockPaper {
   public static void main(String[] args) {
+    staticFileLocation("/public");
     String layout = "templates/layout.vtl";
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
