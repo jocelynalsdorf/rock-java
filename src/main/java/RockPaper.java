775b75ddbfac678edd7e12b98bab10423666402a
jocelyn alsdorf & colleen minor & Teresa Fletcher <student@epicodus.com>
2015-08-12 15:41:44 -0700

    All all integration tests pass

--- src/main/java/RockPaper.java
+++ src/main/java/RockPaper.java
@@ -10,15 +10,29 @@ public class RockPaper {
   public static void main(String[] args) {
     String layout = "templates/layout.vtl";
     get("/", (request, response) -> {
-      Map<String, Object> model = new Hashmap<String, Object>();
+      Map<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/home.vtl");
-      
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/whowon", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/whowon.vtl");
+
+      String playerOne = request.queryParams("playerOne");
+      String playerTwo = request.queryParams("playerTwo");
+
+      String whoWins = whoWins(playerOne, playerTwo);
+
+      model.put("playerOne", playerOne);
+      model.put("playerTwo", playerTwo);
+      model.put("whoWins", whoWins(playerOne, playerTwo));
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
   }
 
-    public String whoWins(String playerOne, String playerTwo)  {
+    public static String whoWins(String playerOne, String playerTwo)  {
 
     String player1= playerOne.toLowerCase();
     String player2= playerTwo.toLowerCase();
